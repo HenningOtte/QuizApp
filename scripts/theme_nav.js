f4a7d1ca5543f076998f4ec78b5a833423d213c4
HenningOtte <HenningOtte91@.com>
2025-05-10 18:55:00 +0200

    change script.js

--- scripts/theme_nav.js
+++ scripts/theme_nav.js
@@ -1,9 +1,7 @@
 function open_theme(index) {
     const theme = document.querySelectorAll('.theme')[index];
     const theme_top = theme.getBoundingClientRect().top;
-    const main_top = document.getElementById('main_container').getBoundingClientRect().top;
-    const quiz_box = document.getElementById('quiz_box');
-    let newPos = theme_top - main_top;    
-    quiz_box.style.top = `${newPos}px`;
+    const main_top = document.getElementById('main_container').getBoundingClientRect().top;   
+    quiz_box.style.top = `${theme_top - main_top}px`;
     quiz_box.innerHTML = theme.innerHTML;
 };
